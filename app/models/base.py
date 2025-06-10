ba5e62a88176ac57236c0a93fc3448a90ed8329d
codiantdivyanshu <divyanshu.gupta@codiant.com>
2025-06-10 11:08:17 +0530

    Update base.py

--- app/models/base.py
+++ app/models/base.py
@@ -1,1 +1,16 @@
+from abc import ABC, abstractmethod 
+from datetime import datetime
+from typing import Optional 
+import uuid 
 
+class BaseEntity (ABC):
+  """Abstract base class for all entities"""
+
+  def __init__(self,name:, str):
+    self._id: str(uuid.uuid4())
+    self._created_at: datetime = datetime.now()
+    self._updated_at: datetime = datetime.now()
+
+  @property 
+  def id(self) -> str:
+    return self._id 
